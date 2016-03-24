108e8d4b038e2a4c55250bf0311fc170c7165b92
12 <pierrick.rambaud@orange.fr>
2016-03-24 15:22:55 +0100

    drag&drop js

---
+++ BDS/web/js/drag_n_drop.js
@@ -0,0 +1,89 @@
+/**
+ * Namespace contenant les methodes que nous allons 
+ * créer pour le système de drag&drop
+ */
+var dndHandler ={
+	
+	//propriété pointant vers l'élément en cours de déplacement 
+	draggedElement: null,
+		
+	applyDragEvents: function(element){
+		
+		element.draggable = true;
+		
+		//variable necessaire pour l'event dragstart (facilite l'accès au namespace)
+		var dndHandler = this;
+		
+		element.addEventListener('dragstart', function(e){
+			//on stoppe la propagation de l'évènement pour empecger une zone de drop d'agir
+			e.stopPropagation();
+			
+			//sauvegarde de l'élément en cours de drag
+			dndHandler.draggedElement = e.target;
+			
+			//c'est cadeau juste pour fireFox
+			e.dataTransfer.setData('text/plain', '');
+		});
+	},
+	
+	applyDropEvents: function(dropper){
+		
+		dropper.addEventListener('dragover', function(e){
+			
+			//on autorise le drop d'éléments
+			e.preventDefault();
+			
+			//on met l'élément en surbrillance 
+			
+		
+		});
+	
+		
+		dropper.addEventListener('dragleave', function(e){
+			
+			//on revient au style de départ 
+			
+			
+		});
+		
+		dropper.addEventListener('drop', function(e){
+			
+			var target = e.target;
+			var draggedElement = dndHandler.draggedElement; //recuperation de l'élément draggé
+			var cloneElement = draggedElement.cloneNode(true); //on crée immédiatement le clone de cette élément 
+			
+			while (target.id.indexOf('dropper') == -1 ){
+				target = target.parentNode;
+			}
+			//application du style par défaut 
+			
+			//ajout de l'élément cloné à la zone de drop actuelle
+			cloneElement = target.appendChild(cloneElement);
+			
+			//nouvelle application des évènements qui ont été perdus lors du clonage
+			dndHandler.applyDragEvents(cloneElement);
+			
+			//suppression de l'élément d'origine 
+			draggedElement.parentNode.removeChild(draggedElement);
+			
+		});
+	}		
+};
+
+var elements = $('#dropzone p');
+var elementsLen = elements.length;
+
+for (var i = 0; i< elementsLen; i++){
+	//application des parametres aux éléments déplacables
+	dndHandler.applyDragEvents(elements[i]);
+}
+
+var droppers = $('#dropper_oui, #dropper_non, #dropper_aucun');
+var droppersLen = droppers.length;
+
+for (var i = 0; i < droppersLen; i++){
+	//application des paramettres aux éléments de drop
+	dndHandler.applyDropEvents(droppers[i]);
+}
+	
+	
