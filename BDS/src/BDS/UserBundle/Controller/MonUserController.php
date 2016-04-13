758a89d379555ed2d15d413fc4d8cc132f7ca978
12rambau <pierrick.rambaud@orange.fr>
2016-04-13 10:05:35 +0200

    bug de edit profile close #3

--- BDS/src/BDS/UserBundle/Controller/MonUserController.php
+++ BDS/src/BDS/UserBundle/Controller/MonUserController.php
@@ -87,4 +87,21 @@ class MonUserController  extends Controller
 		));
 		
 	}
+	
+	public function showAction(User $user=null)
+	{
+		if( $user == null ) {
+			$user = $this->get('security.context')->getToken()->getUser();
+			if (!is_object($user) || !$user instanceof UserInterface) {
+				throw new AccessDeniedException('This user does not have access to this section.');
+			}
+		}
+	
+		//on récupère les évènements auquels participe l'utilisateur
+			
+		return $this->render('BDSUserBundle:Profile:show_content.html.twig', array(
+				'user' => $user
+	
+		));
+	}
 }
