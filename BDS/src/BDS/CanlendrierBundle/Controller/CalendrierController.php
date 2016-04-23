dd3998bf0b47027715e6c8a80398c5bc29fe1d9a
12rambau <pierrick.rambaud@orange.fr>
2016-04-23 23:35:59 +0200

    controller du dashboard

---
+++ BDS/src/BDS/CanlendrierBundle/Controller/CalendrierController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BDS\CanlendrierBundle\Controller;
+
+use BDS\UserBundle\Entity\User;
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class CalendrierController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('BDSCanlendrierBundle:Default:index.html.twig', array('name' => $name));
+    }
+    
+    public function DashboardAction()
+    {
+    	$user = $this->get('security.context')->getToken()->getUser();
+    		
+    	if (!is_object($user) || !$user instanceof UserInterface) {
+    		throw new AccessDeniedException('This user does not have access to this section.');
+    	}
+    	
+    	return $this->render('BDSCalendrierBundle:calendrier:dahboard.html.twig', array(
+    			'user'	=>	$user
+    	));
+    }
+}
