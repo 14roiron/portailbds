17766492c0b714433f8c32722eab28409a95084c
Rambaud Pierrick <12rambau@users.noreply.github.com>
2016-04-25 16:48:45 +0200

    mise en place du squelettes

--- BDS/src/BDS/CalendrierBundle/Resources/Public/js/fullCalSport.js
+++ BDS/src/BDS/CalendrierBundle/Resources/Public/js/fullCalSport.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
-  
-  
+    
+    //on part sur la semaine suivante 
+    $("[id=next_week]").click(function(){
+        //afficher le loader
+        
+        // on récupère la date du prochain lundi 
+        
+        //charger le tableau superieur pour changer les dates des jours 
+        
+        //vider les évènements 
+        
+        //chargers les évènements avec ajax
+        
+        //les placer dans le nouveau calendrier
+        
+        //enlever le loader
+        
+        //recharger la barre d'aujourdhui 
+        
+    });
 }
